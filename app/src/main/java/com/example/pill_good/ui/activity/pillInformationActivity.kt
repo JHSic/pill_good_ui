9f2317e434e734494ecf3b553b7a20ceb9367532
JHSic <joohoo621@kumoh.ac.kr>
2023-05-17 15:38:20 +0900

    알약 세부정보 화면 - constraint Layout에서 scroll로 변경

--- app/src/main/java/com/example/pill_good/ui/activity/pillInformationActivity.kt
+++ app/src/main/java/com/example/pill_good/ui/activity/pillInformationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pill_good.ui.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,14 +16,9 @@ class pillInformationActivity : AppCompatActivity(){
 
         val closeButton : ImageButton = findViewById(R.id.pill_information_close_btn) // 닫기 버튼
         val pillInformationName : TextView = findViewById(R.id.pill_information_name) // 약 정보 화면 약 이름
-        val pillEffectInformation : TextView = findViewById(R.id.pill_information_effect) // 약 정보 화면 약 효능
-        val pillPrecautionInformation : TextView = findViewById(R.id.pill_information_precaution) // 약 정보 화면 약 주의사항
+        val pillEffectInformation : TextView = findViewById(R.id.pill_information_effect_content) // 약 정보 화면 약 효능
+        val pillPrecautionInformation : TextView = findViewById(R.id.pill_information_precaution_content) // 약 정보 화면 약 주의사항
 
-        pillEffectInformation.isSelected = true
-        pillEffectInformation.requestFocus()
-
-        pillPrecautionInformation.isSelected = true
-        pillPrecautionInformation.requestFocus()
 
         closeButton.setOnClickListener {
             finish()
