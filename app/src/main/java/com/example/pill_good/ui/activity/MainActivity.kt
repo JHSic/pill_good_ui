e39df357a0fe0526ed32862c3b0dee95171dff04
Dong-hui-Kim <donghui11777@gmail.com>
2023-06-04 23:52:18 +0900

    Feat: "복용 현황 정보에서 약의 이미지와 속성 정보 출력 구현"
    MockData도 그에 맞게 수정, 이미지 캐시처리로 로드한 이미지에 대해서 재로딩 되지 않게 수정

--- app/src/main/java/com/example/pill_good/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/pill_good/ui/activity/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
-import com.bumptech.glide.Glid
+import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.pill_good.R
 import com.example.pill_good.data.dto.MedicationInfoDTO
